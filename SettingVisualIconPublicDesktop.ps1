1c32afd631b041982ec62146d8481b9216bd757f
Seth Marcus <seth.marcus07@qmail.cuny.edu>
2021-11-29 15:43:15 -0500

    Make Visaul on public desktop with icon

---
+++ SettingVisualIconPublicDesktop.ps1
@@ -0,0 +1,17 @@
+﻿set executionpolicy bypass –force
+
+$desktop = "C:\Users\Public\Desktop" 
+if(!Test-Path $desktopA){
+    $desktop = "C:\Users\Public\Public Desktop"
+}
+
+# Create shortcut to the file
+$SourceVisualEMR = "C:\rhs\visual\emr\amd64\ie8\Visual_IE8.bat"
+$ShortcutVisualEMR = "$($desktop)\VisaulEMR.lnk"
+
+$WScriptObj = New-Object -ComObject ("WScript.Shell")
+$shortcut = $WscriptObj.CreateShortcut($ShortcutVisualEMR)
+$shortcut.TargetPath = $SourceVisualEMR
+$shortcut.IconLocation = "C:\rhs\Visual\emr\<something>.ico"
+$shortcut.Save()
+
